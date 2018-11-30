6cbe4eef35aa61d4dbe77b9075d7901e91d126e7
Philippe SIM <psim@e3r12p21.42.fr>
2018-11-30 15:49:28 +0100

    updated read_ttrm_file.c

--- read_ttrm_file.c
+++ read_ttrm_file.c
@@ -6,30 +6,76 @@
 /*   By: psim <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 15:14:37 by psim              #+#    #+#             */
-/*   Updated: 2018/11/29 14:59:23 by psim             ###   ########.fr       */
+/*   Updated: 2018/11/30 15:49:11 by psim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*t_tetriminos	**fd_to_tetriminos_tab(int fd)
+t_ttrm	**fd_to_ttrm_tab(int fd)
 {
 	int ret;
-	char buf[5];
-	t_tetriminos *tab = (t_tetriminos*) malloc(sizeof(t_tetriminos*) * 27);
-	if (tab == NULL)
-	{
-		return (NULL):
-	}
+	int i;
+	char buf[1];
 
-	while ((ret = read(fd, buf, 5)) > 0)
+	t_ttrm **tab = (t_ttrm**)malloc(sizeof(t_ttrm*) * 27);
+	if (tab == NULL)
+		return (NULL);
+	i = 0;
+	while (fill_ttrm(fd, &(tab[i])))
 	{
-		//si erreur free tous les tetriminos deja alloues + return NULL
+		ret = read(fd, buf, 1);
+		if (ret = 0)
+			return (0);
+
+/*
+a finir
 */
 
-int		fill_tetriminos(int fd, s_tetriminos **new_tetriminos)
+static int	check_line(char *str)
 {
-	i = 4;
+	int i;
 
-
-	while(i)
+	i = 0;
+	while (str[i] != '\0')
 	{
+		if (str[i] != '#' || str[i] != '.')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
+int		fill_ttrm(int fd, s_ttrm **new_ttrm)
+{
+	int		i;
+	int		y;
+	char	buf[5];
+	int		ret;
+
+
+	x = 0;
+	y = 0;
+	if (!(*new_ttrm = (t_ttrm*)malloc(sizeof(t_ttrm) * 1)))
+		return (NULL);
+	ft_bzero(buf, 5);
+	while (y < 4)
+	{
+		ret = read(fd, buf, 5);
+		if (ret != 5 || bf[4] != '\n' || !check_line(buf))
+		{
+			free(*new_ttrm);
+			return (-1);
+		}
+		ft_strncpy(new_ttrm->tab[y], buf, 4);
+		y++;
+		ft_bzero(buf, 5);
+	}
+	ret = read(fd, buf, 1);
+	if (ret == 0)
+		return (0);
+	if (ret == -1 || buf[0] != '\n')
+	{
+		free(*new_ttrm);
+		return (-1);
+	}
+	return (1);
+}
