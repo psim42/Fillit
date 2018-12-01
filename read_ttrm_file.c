ab0b0b9da92bd86a04a9dfd36ad2b3e52f67fd5f
Philippe SIM <psim@e2r13p9.42.fr>
2018-12-01 14:22:38 +0100

    fixed read_ttrm_file.c

--- read_ttrm_file.c
+++ read_ttrm_file.c
@@ -6,13 +6,14 @@
 /*   By: psim <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 16:49:30 by psim              #+#    #+#             */
-/*   Updated: 2018/11/30 17:01:43 by psim             ###   ########.fr       */
+/*   Updated: 2018/12/01 14:22:07 by psim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "fillit.h"
 #include <stdlib.h>
+#include <unistd.h>
 
 t_ttrm	**fd_to_ttrm_tab(int fd)
 {
@@ -20,7 +21,7 @@ t_ttrm	**fd_to_ttrm_tab(int fd)
 	int i;
 	char buf[1];
 
-	t_ttrm **tab = (t_ttrm**)malloc(sizeof(t_ttrm*) * 27);
+	t_ttrm **tab = (t_ttrm**)ft_memalloc(sizeof(t_ttrm*) * 27);
 	if (tab == NULL)
 		return (NULL);
 	i = 0;
@@ -39,16 +40,16 @@ static int	check_line(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (i < 4)
 	{
-		if (str[i] != '#' || str[i] != '.')
+		if (str[i] != '#' && str[i] != '.')
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
-int		fill_ttrm(int fd, s_ttrm **new_ttrm)
+int		fill_ttrm(int fd, t_ttrm **new_ttrm)
 {
 	int		i;
 	int		y;
@@ -56,30 +57,30 @@ int		fill_ttrm(int fd, s_ttrm **new_ttrm)
 	int		ret;
 
 
-	x = 0;
 	y = 0;
-	if (!(*new_ttrm = (t_ttrm*)malloc(sizeof(t_ttrm) * 1)))
-		return (NULL);
+	if (!(*new_ttrm = (t_ttrm*)ft_memalloc(sizeof(t_ttrm) * 1)))
+		return (-1);
 	ft_bzero(buf, 5);
 	while (y < 4)
 	{
 		ret = read(fd, buf, 5);
-		if (ret != 5 || bf[4] != '\n' || !check_line(buf))
+		if (ret != 5 || buf[4] != '\n' || !check_line(buf))
 		{
-			free(*new_ttrm);
+			ft_memdel((void **)new_ttrm);
 			return (-1);
 		}
-		ft_strncpy(new_ttrm->tab[y], buf, 4);
+		ft_strncpy((*new_ttrm)->tab[y], buf, 4);
 		y++;
 		ft_bzero(buf, 5);
 	}
 	ret = read(fd, buf, 1);
-	if (ret == 0)
-		return (0);
-	if (ret == -1 || buf[0] != '\n')
+	if (ret == -1 || (buf[0] != '\n' && ret > 0))
 	{
-		free(*new_ttrm);
+		ft_memdel((void **)new_ttrm);
 		return (-1);
 	}
+	normalize_ttrm(*new_ttrm);
+	if (ret == 0)
+		return (0);
 	return (1);
 }
