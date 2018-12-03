ee45f92c1252d3b230cb6b0141937577f4f55e2a
Philippe SIM <psim@e2r12p23.42.fr>
2018-12-03 13:50:51 +0100

    a

--- insert_ttrm.c
+++ insert_ttrm.c
@@ -1,31 +1,77 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   emplace_ttrm.c                                     :+:      :+:    :+:   */
+/*   insert_ttrm.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psim <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/01 15:13:37 by psim              #+#    #+#             */
-/*   Updated: 2018/12/03 12:16:02 by psim             ###   ########.fr       */
+/*   Created: 2018/12/03 13:07:03 by psim              #+#    #+#             */
+/*   Updated: 2018/12/03 13:50:32 by psim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		emplace_ttrm(t_map *map, t_ttrm *ttrm, int x, int y)
+static void	abort_ttrm(t_map *map, t_ttrm *ttrm, t_coord cd, int i)
 {
 	int tx;
 	int ty;
+	int i;
+
+	while (ty < 4 && i)
+	{
+		while (tx < 4 && i)
+		{
+			if (ttrm->tab[ty][tx] != '.' && map->tab[y + ty][x + tx] != '.')
+			{
+				map->tab[y + ty][x + tx] = '.';
+				i--;
+			}
+			tx++;
+		}
+		ty++;
+	}
+}
+
+static void	coord_struct(int x, int y)
+{
+	t_coord cd;
+
+	cd->x = x;
+	cd->y = y;
+	return (cd);
+}
+
+int			insert_ttrm(t_map *map, t_ttrm *ttrm, int x, int y)
+{
+	int tx;
+	int ty;
+	int i;
 
 	tx = 0;
 	ty = 0;
-	while (ttrm->tab[ty] < 4)
+	i = 0;
+	while (ty < 4)
 	{
-		while (ttrm->tab[tx] < 4)
+		while (tx < 4)
 		{
-			if (map->tab[x] == '.')
-				map->tab[x] = ttrm->tab[tx];
-			if (tx >= map->size && ttrm->tab[tx] != '.')
+			if (ttrm->tab[ty][tx] != '.' && (x + tx >= map->size
+						|| y + ty >= map->size))
 			{
-				remove_ttrm(/* */);
+				abort_ttrm(map, ttrm, coord_struct(x, y), i);
 				return (0);
 			}
-
+			if (ttrm->tab[ty][tx] != '.' && map->tab[y + ty][x + tx] == '.')
+			{
+				map->tab[y + ty][x + tx] = ttrm->tab[tx];
+				i++;
+			}
+			else
+			{
+				abort_ttrm(map, ttr, coord_struct(x, y), i);
+				return (0);
+			}
+			tx++;
+		}
+		ty++;
+	}
+	return (1);
+}
