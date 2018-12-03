8ca14d8ca3e681b5bdf242a935915a840fa36373
Philippe SIM <psim@e2r12p23.42.fr>
2018-12-03 14:12:48 +0100

    a

--- print_map.c
+++ print_map.c
@@ -6,7 +6,7 @@
 /*   By: psim <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 14:48:22 by psim              #+#    #+#             */
-/*   Updated: 2018/12/03 11:50:40 by fwerner          ###   ########.fr       */
+/*   Updated: 2018/12/03 14:12:42 by psim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,19 @@
 
 void	print_map(t_map *map)
 {
+	int x;
+	int y;
+
+	x = 0;
+	y = 0;
+	while (y < map->size)
+	{
+		while (x < map->size)
+		{
+			ft_putchar(map->tab[y][x]);
+			ft_putchar('\n');
+			x++;
+		}
+		y++;
+	}
 }
